1360b67d74dfd611d97b58c4dfa90f2aeb46b2bf
Jaime RGP <jaimergp@users.noreply.github.com>
2021-11-20 13:08:37 +0100

    support alphas and other non-int minor versions

--- constructor/conda_interface.py
+++ constructor/conda_interface.py
@@ -16,7 +16,12 @@ except ImportError:
                        "with sys.prefix: %s" % sys.prefix)
 
 if conda_interface_type == 'conda':
-    CONDA_MAJOR_MINOR = tuple(int(x) for x in CONDA_INTERFACE_VERSION.split('.')[:2])
+    from conda.models.version import VersionOrder
+
+    _conda_version = VersionOrder(CONDA_INTERFACE_VERSION)
+    _conda_major = _conda_version[1]
+    _conda_minor = _conda_version[2][0] if isinstance(_conda_version[2], list) else _conda_version[2]
+    CONDA_MAJOR_MINOR = _conda_major, _conda_minor
 
     from conda._vendor.toolz.itertoolz import (
         concatv as _concatv, get as _get, groupby as _groupby,
