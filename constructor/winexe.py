e2c4684907579e59d4bd575b9d934fb1bdae8df2
Jaime RGP <jaimergp@users.noreply.github.com>
2021-12-23 14:01:46 +0100

    better timestamping on windows

--- constructor/winexe.py
+++ constructor/winexe.py
@@ -232,8 +232,8 @@ def create(info, verbose=False):
     if pfx_certificate:
         signtool = os.environ.get("CONSTRUCTOR_SIGNTOOL_PATH", "signtool")
         password = os.environ.get("CONSTRUCTOR_PFX_CERTIFICATE_PASSWORD")
-        timestamp_server = os.environ.get("CONSTRUCTOR_SIGNTOOL_TIMESTAMP_SERVER_URL", "http://timestamp.comodoca.com/authenticode")
-        args = [signtool, "sign", "/f", pfx_certificate, "/t", timestamp_server]
+        timestamp_server = os.environ.get("CONSTRUCTOR_SIGNTOOL_TIMESTAMP_SERVER_URL", "http://timestamp.sectigo.com")
+        args = [signtool, "sign", "/f", pfx_certificate, "/tr", timestamp_server, "/td", "sha256", "/fd", "sha256"]
         if password:
             args += ["/p", password]
         args.append(info["_outpath"])
